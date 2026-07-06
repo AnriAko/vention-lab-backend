dafb5c63aa35f1098f55520850181b931a282589
AnriAko <ejik.v.tu.m.ane97@gmail.com>
2026-07-06 12:27:11 +0400

    fix: DI injection issues

--- src/modules/organization/organization.module.ts
+++ src/modules/organization/organization.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OrganizationService } from './organization.service';
 import { OrganizationController } from './organization.controller';
+import { OrganizationsRepository } from '~/modules/organization/organization.repository';
+import { PrismaModule } from '~/infrastructure/database/prisma.module';
 
 @Module({
+    imports: [PrismaModule],
     controllers: [OrganizationController],
-    providers: [OrganizationService],
+    providers: [OrganizationService, OrganizationsRepository],
 })
 export class OrganizationModule {}
