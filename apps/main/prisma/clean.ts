65db00287fc6ad89d84bd6496abeee445512dcc9
AnriAko <ejik.v.tu.m.ane97@gmail.com>
2026-09-14 13:53:57 +0400

    fix: seeding script

--- apps/main/prisma/clean.ts
+++ apps/main/prisma/clean.ts
@@ -5,17 +5,25 @@ import { prisma, disconnect } from './client';
 async function main() {
     console.log('START CLEAN');
 
-    await prisma.$transaction([
-        prisma.message.deleteMany(),
-        prisma.usersChats.deleteMany(),
-        prisma.file.deleteMany(),
-        prisma.chat.deleteMany(),
-        prisma.usersOrganizationsRoles.deleteMany(),
-        prisma.usersOrganizations.deleteMany(),
-        prisma.owner.deleteMany(),
-        prisma.user.deleteMany(),
-        prisma.organization.deleteMany(),
-    ]);
+    await prisma.$transaction(
+        [
+            prisma.aiConversationMessage.deleteMany(),
+            prisma.aiConversation.deleteMany(),
+
+            prisma.message.deleteMany(),
+            prisma.usersChats.deleteMany(),
+            prisma.file.deleteMany(),
+            prisma.chat.deleteMany(),
+            prisma.usersOrganizationsRoles.deleteMany(),
+            prisma.usersOrganizations.deleteMany(),
+            prisma.owner.deleteMany(),
+            prisma.user.deleteMany(),
+            prisma.organization.deleteMany(),
+        ],
+        {
+            timeout: 10_000,
+        }
+    );
 
     console.log('DATABASE CLEANED');
 }
